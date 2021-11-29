3e004d6346398986a4eda3f5d538f2577557250e
m.tatibaev <tatibaevm@vezitaxi.com>
2021-11-29 16:50:34 +0500

    Core app cleared

--- app/core/urls.py
+++ app/core/urls.py
@@ -2,19 +2,12 @@ from django.urls import path, include
 
 from .views import AccountViewSet, ChangeBalance
 from rest_framework import routers
-from rest_framework import renderers
-from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
-from rest_framework.urlpatterns import format_suffix_patterns
 
 
 app_name = 'account'
 
 router = routers.DefaultRouter()
 router.register(r'account', AccountViewSet)
-# router.register(r'account', AccountViewSet)
 
 
 urlpatterns = [
